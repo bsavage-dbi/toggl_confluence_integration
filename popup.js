16909bf07b33dc7bfd5110d13ab15024346a1668
Tom Van Rossom <tom.vanrossom@fluidda.com>
2018-03-12 08:45:09 +0100

    add chrome promises library => get current tab

--- popup.js
+++ popup.js
@@ -11,42 +11,19 @@ const chromep = new ChromePromise();
  *   is found.
  */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
-  var queryInfo = {
-    active: true,
-    currentWindow: true
-  };
+    // Query filter to be passed to chrome.tabs.query - see
+    // https://developer.chrome.com/extensions/tabs#method-query
+    var queryInfo = {
+        active: true,
+        currentWindow: true
+    };
 
-  chrome.tabs.query(queryInfo, (tabs) => {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
-    var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
-    var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-
-    callback(url);
-  });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, (tabs) => {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+    return chromep.tabs.query(queryInfo)
+        .then((tabs) => {
+            return Promise.resolve(tabs[0].url);})
+        .then(function (url) {
+            console.assert(typeof url == 'string', 'tab.url should be a string');
+            return url});
 }
 
 /**
@@ -146,7 +123,7 @@ function startTimer() {
             messageElement.innerHTML = 'Timer started! ' + JSON.parse(xhr.responseText).data.description;
           }
         };
-        getCurrentTabUrl((url) => {
+        getCurrentTabUrl().then(function (url) {
             lookUpProject((pid) => {
                 var body = {
                     "time_entry": {
@@ -173,7 +150,7 @@ function lookUpProject(callback){
                     var projects = JSON.parse(xhr.responseText);
                     //alert('found projects:' + projects);
 
-                    getCurrentTabUrl((url) => {
+                    getCurrentTabUrl().then(function (url) {
                         var currentProjectName = extractProjectName(url);
                         if(currentProjectName){
 
@@ -262,7 +239,7 @@ function getWorkspaceId(callback) {
 // user devices.
 document.addEventListener('DOMContentLoaded', () => {  
   
-	getCurrentTabUrl((url) => {
+	getCurrentTabUrl().then(function (url) {
 	  
 		var timerButton = document.getElementById('startTimer');
 		timerButton.addEventListener('click', startTimer); 
