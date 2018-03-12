587b68e4d10786df644bc3f78a9a35ff8778d3d1
Tom Van Rossom <tom.vanrossom@fluidda.com>
2018-03-12 10:18:34 +0100

    add chrome promises library => auto indent source

--- popup.js
+++ popup.js
@@ -19,37 +19,22 @@ function getCurrentTabUrl(callback) {
     };
 
     return chromep.tabs.query(queryInfo)
-        .then((tabs) => {
-            return Promise.resolve(tabs[0].url);})
+        .then(function (tabs) {
+            return Promise.resolve(tabs[0].url);
+        })
         .then(function (url) {
             console.assert(typeof url == 'string', 'tab.url should be a string');
-            return url});
+            return url
+        });
 }
 
-/**
- * Change the background color of the current page.
- *
- * @param {string} color The new background color.
- */
-function changeBackgroundColor(color) {
-  var script = 'document.body.style.backgroundColor="' + color + '";';
-  // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-  // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-  // into a page. Since we omit the optional first argument "tabId", the script
-  // is inserted into the active tab of the current window, which serves as the
-  // default.
-  chrome.tabs.executeScript({
-    code: script
-  });
-}
-
-function selectJiraSummary(){
+function selectJiraSummary() {
     var summary = document.getElementById('summary-val').innerText;
     console.log('Tab script: ' + summary);
     return summary;
 }
 
-function retrieveJiraSummary(){
+function retrieveJiraSummary() {
     return chromep.tabs.executeScript({
         code: '(' + selectJiraSummary + ')();' //argument here is a string but function.toString() returns function's code
     }).then(function (results) {
@@ -59,42 +44,13 @@ function retrieveJiraSummary(){
     });
 }
 
-/**
- * Gets the saved background color for url.
- *
- * @param {string} url URL whose background color is to be retrieved.
- * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
- */
-function getSavedBackgroundColor(url, callback) {
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-  // for chrome.runtime.lastError to ensure correctness even when the API call
-  // fails.
-  chrome.storage.sync.get(url, (items) => {
-    callback(chrome.runtime.lastError ? null : items[url]);
-  });
-}
-
-/**
- * Sets the given background color for url.
- *
- * @param {string} url URL for which background color is to be saved.
- * @param {string} color The background color to be saved.
- */
-function saveBackgroundColor(url, color) {
-  var items = {};
-  items[url] = color;
-  // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-  // optional callback since we don't need to perform any action once the
-  // background color is saved.
-  chrome.storage.sync.set(items);
-}
-
-function saveApiToken(){
-	var token = document.getElementById('personalApiKey').value;
+function saveApiToken() {
+    var token = document.getElementById('personalApiKey').value;
 
     chromep.storage.sync.set({"token": token})
-        .then(function () {console.log('Saved token: ' + token)  });
+        .then(function () {
+            console.log('Saved token: ' + token)
+        });
 }
 
 function getAuthorizationHeader() {
@@ -110,7 +66,7 @@ function startTimer() {
     var taskDescriptionPromise = extractTaskDescription();
 
     Promise.all([taskDescriptionPromise, projectIdPromise, authorizationHeaderPromise])
-        .then(function(values) {
+        .then(function (values) {
             console.log('Promise returned: ' + values);
 
             var taskDescription = values[0];
@@ -145,52 +101,52 @@ function startTimer() {
         });
 }
 
-function lookUpProject(){
+function lookUpProject() {
     var authorizationHeaderPromise = getAuthorizationHeader();
     var workspaceIdPromise = getWorkspaceId();
 
     var projectsPromise = Promise.all([authorizationHeaderPromise, workspaceIdPromise])
-        .then(function(values) {
-            console.log('Promise returned: '+values);
+        .then(function (values) {
+            console.log('Promise returned: ' + values);
 
             var headerValue = values[0];
             var wid = values[1];
 
             return new Promise(function (resolve, reject) {
                 var xhr = new XMLHttpRequest();
-                xhr.open("GET", "https://www.toggl.com/api/v8/workspaces/"+wid+"/projects", true);
+                xhr.open("GET", "https://www.toggl.com/api/v8/workspaces/" + wid + "/projects", true);
                 xhr.setRequestHeader("Authorization", headerValue);
                 xhr.onload = resolve;
                 xhr.onerror = reject;
                 xhr.send();
             }).then(function (e) {
-                console.log('lookUpProject succes: '+e.target.response);
+                console.log('lookUpProject succes: ' + e.target.response);
                 return JSON.parse(e.target.response)
             }, function (e) {
-                console.log('lookUpProject error: '+e);
+                console.log('lookUpProject error: ' + e);
             });
 
         });
     var currentTabUrlPromise = getCurrentTabUrl();
 
     return Promise.all([projectsPromise, currentTabUrlPromise])
-        .then(function(values) {
-            console.log('Promise returned: '+values);
+        .then(function (values) {
+            console.log('Promise returned: ' + values);
 
             var projects = values[0];
             var url = values[1];
 
             var currentProjectName = extractProjectName(url);
-            if(currentProjectName){
+            if (currentProjectName) {
 
                 var filtered = projects
-                    .filter(function(project){
+                    .filter(function (project) {
                         return currentProjectName.toLowerCase() == project.name.toLowerCase();
                     });
-                if(filtered.length==0){
-                    console.log('No Project found in toggle with name: '+ currentProjectName);
+                if (filtered.length == 0) {
+                    console.log('No Project found in toggle with name: ' + currentProjectName);
                     return false;
-                }else{
+                } else {
                     return filtered[0].id;
                 }
             }
@@ -198,25 +154,25 @@ function lookUpProject(){
         });
 }
 
-function extractProjectName(url){
-	//alert(url.indexOf('https://confluence.fluidda.com/display/'));
-	if(url.indexOf('https://confluence.fluidda.com/display/') !== -1){
-		return url.split('/')[4];
-	}else if(url.indexOf('https://jira.fluidda.com/projects/') !== -1){
-		return url.split('/')[4];
-	}else if(url.indexOf('https://jira.fluidda.com/browse/') !== -1){
-		return url.split('/')[4].split('-')[0];
-	}else{
-		return null;
-	}
+function extractProjectName(url) {
+    //alert(url.indexOf('https://confluence.fluidda.com/display/'));
+    if (url.indexOf('https://confluence.fluidda.com/display/') !== -1) {
+        return url.split('/')[4];
+    } else if (url.indexOf('https://jira.fluidda.com/projects/') !== -1) {
+        return url.split('/')[4];
+    } else if (url.indexOf('https://jira.fluidda.com/browse/') !== -1) {
+        return url.split('/')[4].split('-')[0];
+    } else {
+        return null;
+    }
 }
 
-function extractTaskDescription(){
+function extractTaskDescription() {
     return getCurrentTabUrl()
         .then(function (url) {
-            if(url.indexOf('https://confluence.fluidda.com/display/') !== -1){
+            if (url.indexOf('https://confluence.fluidda.com/display/') !== -1) {
                 return new Promise(url.split('/')[5]);
-            }else if(url.indexOf('https://jira.fluidda.com/browse/') !== -1){
+            } else if (url.indexOf('https://jira.fluidda.com/browse/') !== -1) {
                 return retrieveJiraSummary()
                     .then(function (summary) {
                         var description = url.split('/')[4];
@@ -226,7 +182,7 @@ function extractTaskDescription(){
                         return description;
                     });
 
-            }else{
+            } else {
                 return new Promise('');
             }
         });
@@ -235,40 +191,40 @@ function extractTaskDescription(){
 function getCurrentTimeEntry() {
     return getAuthorizationHeader()
         .then(function (headerValue) {
-            return new Promise(function (resolve, reject) {
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", "https://www.toggl.com/api/v8/time_entries/current");
-                xhr.setRequestHeader("Authorization", headerValue);
-                xhr.onload = resolve;
-                xhr.onerror = reject;
-                xhr.send();
-            });
-        }
-    ).then(function (e) {
-        console.log('getCurrentTimeEntry succes: '+e.target.response);
-        return JSON.parse(e.target.response).data
-    }, function (e) {
-        console.log('getCurrentTimeEntry error: '+e);
-    });
+                return new Promise(function (resolve, reject) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", "https://www.toggl.com/api/v8/time_entries/current");
+                    xhr.setRequestHeader("Authorization", headerValue);
+                    xhr.onload = resolve;
+                    xhr.onerror = reject;
+                    xhr.send();
+                });
+            }
+        ).then(function (e) {
+            console.log('getCurrentTimeEntry succes: ' + e.target.response);
+            return JSON.parse(e.target.response).data
+        }, function (e) {
+            console.log('getCurrentTimeEntry error: ' + e);
+        });
 }
 
 function getWorkspaceId() {
-	return chromep.storage.sync.get("wid").then(function(data) {
-		if (typeof data.wid === 'undefined') {
-			alert('Not found in local storage')
-			return getCurrentTimeEntry()
-                .then(function(entry){
-                    if(entry){
+    return chromep.storage.sync.get("wid").then(function (data) {
+        if (typeof data.wid === 'undefined') {
+            alert('Not found in local storage')
+            return getCurrentTimeEntry()
+                .then(function (entry) {
+                    if (entry) {
                         var workspaceId = entry.wid;
                         chromep.storage.sync.set({"wid": workspaceId});
                     }
                     return workspaceId;
-				} );
-		} else {
-			//alert('Found in local storage')
+                });
+        } else {
+            //alert('Found in local storage')
             return data.wid;
-		}
-	});	
+        }
+    });
 }
 
 // This extension loads the saved background color for the current tab if one
@@ -279,7 +235,7 @@ function getWorkspaceId() {
 // to a document's origin. Also, using chrome.storage.sync instead of
 // chrome.storage.local allows the extension data to be synced across multiple
 // user devices.
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
 
     var timerButton = document.getElementById('startTimer');
     timerButton.addEventListener('click', startTimer);
@@ -289,14 +245,13 @@ document.addEventListener('DOMContentLoaded', function(){
 
     var workspaceIdInput = document.getElementById('workspaceId');
     getWorkspaceId()
-        .then(function(wid){
+        .then(function (wid) {
             workspaceIdInput.value = wid;
-        } );
-
+        });
 
 
     getCurrentTimeEntry()
-        .then(function(entry){
+        .then(function (entry) {
             var messageElement = document.getElementById('userMsg');
             messageElement.innerHTML = entry.description;
         });
